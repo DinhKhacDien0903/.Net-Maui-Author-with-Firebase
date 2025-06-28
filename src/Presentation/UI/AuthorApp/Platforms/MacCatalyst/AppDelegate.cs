67ef28d47a8c02a35f5194a45ffe3423e7b9a2aa
diendk <diendk@newwave.vn>
2025-06-28 19:16:08 +0700

    init presentation with .net maui mobile app

---
+++ src/Presentation/UI/AuthorApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AuthorApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
